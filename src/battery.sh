8970baa42e017014fc18aaaae301494e4bcb45ed
Morgan Murrah <morgan@morganwebdev.com>
2022-12-07 01:15:39 -0800

    Initial Working Script to manage running Curl, Lighthouse and Yellow Lab tools with a bash script. Feeds in from a list in a file called urls.txt

---
+++ src/battery.sh
@@ -0,0 +1,84 @@
+#!/bin/env bash
+set -uo pipefail;
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+case "$(curl -s --max-time 2 -I https://www.google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+  [23]) echo "HTTP connectivity is up";;
+  5) echo "The web proxy won't let us through";;
+  *) err "The network is down or very slow"; exit 1 ;;
+esac
+
+lflag='';
+yflag='';
+sflag='';
+while getopts 'lys' flag; do
+  case "${flag}" in
+    l) lflag='true' ;;
+    y) yflag='true' ;;
+    s) sflag='true' ;;
+    *) err "Unexpected flag"; exit 1 ;;
+  esac
+done
+
+count=0;
+date=$(date); 
+while IFS= read -r line; do
+
+    filename="../output/$count-report.txt";
+
+    curl=$(curl -Is --max-time 15 "$line");
+    res=$?
+    if test "$res" != "0"; then
+        printf "the curl command failed for %s with: %s \n" "$line" "$res" | tee -a "$filename";
+        
+    else
+        printf "The curl command was successful for %s. It exited with a status code of:" "$line" | tee -a "$filename";
+        printf "%s" "$curl" | head -n 1 | tee -a "$filename";
+        printf "\n";
+        printf "\n";
+        printf "COMMENCING ADDITIONAL TESTS:";
+        printf "\n";
+        printf "\n";
+
+        if [[ -n $lflag ]]; then
+            lighthouse $line --output json --output-path ../output/$count-lighthouse-result.json;
+            printf "Global Scores on Lighthouse:"
+            printf "\n";
+            printf "Performance:"
+            printf "\n";
+            jq '.categories.performance.score' ../output/$count-lighthouse-result.json;
+            printf "Accessibility:"
+            printf "\n";
+            jq '.categories.accessibility.score' ../output/$count-lighthouse-result.json;
+            printf "Best Practices:"
+            printf "\n";
+            jq '.categories."best-practices".score' ../output/$count-lighthouse-result.json;
+            printf "SEO:"
+            printf "\n";
+            jq '.categories.seo.score' ../output/$count-lighthouse-result.json;
+        else
+            echo "NO Lighthouse test, try -l next time";
+        fi
+
+        if [[ -n $yflag ]]; then
+            yellowlabtools $line > ../output/$count-yellow-lab-result.json;
+            printf "Global Score on Yellow Labs:"
+            jq '.scoreProfiles.generic.globalScore' ../output/$count-yellow-lab-result.json;
+        else
+            echo "NO YellowLabTools test, try -y next time";
+        fi
+
+        # if [[ -n $sflag ]]; then
+        #     sitespeed.io $line 
+        # else
+        #     echo "NO Sitespeed test, try -s next time";
+        # fi
+
+        (( count++ ));
+
+    fi
+
+done < urls.txt
